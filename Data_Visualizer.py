97d4c57cf5b9d1c0e19c10b685de320b767b8c89
PRunner77 <pjmddc@mail.missouri.edu>
2021-03-10 16:03:29 -0600

    Provided basic structure of python file

---
+++ Data_Visualizer.py
@@ -0,0 +1,36 @@
+#File created 10 March 2021
+
+#Could have a function that checks for user input (no letters, negatives, etc.)
+
+
+
+#Another function could check the end date against beginning date
+
+
+#This could be the main program
+print("""Stock Data-Visualizer
+------------------------\n""")
+
+#User enters stock symbol, may need to be validated
+
+print("""Chart Types
+-------------------
+1. Bar
+2. Line
+""")
+
+#User enters the chart type wanted (either bar or line)
+
+print("""Select the Time Series of the chart you want to Generate
+---------------------------------------------------------
+1. Intraday
+2. Daily
+3. Weekly
+4. Monthly
+""")
+#User inputs the time series wanted between the four options
+
+#Enter the beginning Date (YYYY-MM-DD)
+#Enter the end Date (YYYY-MM-DD)
+
+##Chart will then open in new browser
